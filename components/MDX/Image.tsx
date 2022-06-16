e9cac4391ad30315bb319a42e807cc735140e1dc
Swapnil Kumbhar <swapsk05@gmail.com>
2022-06-15 20:04:40 -0700

    feat(MDX): Integrated with next/image

---
+++ components/MDX/Image.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+type Props = {
+  src: string,
+  alt: string
+};
+
+type MdxLoaderProps = {
+  src: string
+};
+
+const MdxImgLoader = ({ src }: MdxLoaderProps) => `/blog/${src}`;
+
+export default function Img({ src, alt }: Props) {
+  // Height and Width is just aspect ratio, since Layout is "responsive"
+  return (
+    <Image
+      loader={MdxImgLoader}
+      src={src}
+      alt={alt}
+      layout="responsive"
+      width={16}
+      height={12}
+    />
+  );
+}
