ac24e5e3983682b258c79961ddbf5436773eb43c
Swapnil Kumbhar <swapsk05@gmail.com>
2022-07-28 20:47:45 -0700

    fix(components): Fixed styles on Lists and Paragraphs

--- components/MDX/List.tsx
+++ components/MDX/List.tsx
@@ -2,14 +2,14 @@ import DefaultProps from '../types';
 
 export function OrderedList({ children }: DefaultProps) {
   return (
-    <ol className="font-light list-decimal list-inside lg:px-4 lg:py-2 md:px-2 md:py-1">
+    <ol className="text-lg font-light leading-relaxed list-decimal list-inside lg:px-4 lg:py-2 md:px-2 md:py-1">
       {children}
     </ol>
   );
 }
 export function UnorderedList({ children }: DefaultProps) {
   return (
-    <ul className="lg:px-4 lg:py-2 md:px-2 md:py-1 font-light list-[square] list-inside">
+    <ul className="text-lg lg:px-4 lg:py-2 md:px-2 md:py-1 font-light list-[square] list-inside">
       {children}
     </ul>
   );
--- components/MDX/Paragraph.tsx
+++ components/MDX/Paragraph.tsx
@@ -2,7 +2,7 @@ import DefaultProps from '../types';
 
 export default function P({ children }: DefaultProps) {
   return (
-    <div className="py-2 font-light leading-relaxed text-justify text-md md:py-2 lg:text-lg">
+    <div className="py-2 text-lg font-light leading-relaxed md:py-2">
       {children}
     </div>
   );
