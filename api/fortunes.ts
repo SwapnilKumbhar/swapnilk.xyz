0eed915bed359132955489975018cdeb9a28e320
Swapnil Kumbhar <SwapnilKumbhar@users.noreply.github.com>
2024-10-06 18:28:28 -0700

    feat(*): Updated info to 2024 (#25)
    * feat(*): Updated info to 2024
    
    * fix(index): Linted

--- api/fortunes.ts
+++ api/fortunes.ts
@@ -1,47 +1,49 @@
 // All the fortunes. Never let this list stale
 const fortunes = [
-  'Technology. Music. Travel. Whatever.',
+  "Technology. Music. Travel. Whatever.",
   "' 1 = 1 --",
-  'Drink all the things, hack all the booze!',
-  'If you are not very careful, your possessions will possess you.',
-  'Looking for an internship for the Summer of 2022.',
-  '[object Object]',
-  'undefined', // Bet you thought my website was broken ^_^
-  'Use headphones for best experience.',
-  '`base64 /dev/zero` is such a mood.',
-  'My opinions are not my own.',
-  'These fortunes have been cancelled.',
-  'Division is futile. You will be approximated.',
-  ':(){ :|:& };:',
-  'What do you want to debug today?',
-  'I just steal some of these from radare2.',
-  'Causing retinal damage till I figure out dark mode.',
-  '* screams internally *',
+  "Drink all the things, hack all the booze!",
+  "If you are not very careful, your possessions will possess you.",
+  "Was looking for an internship for the Summer of 2022.",
+  "[object Object]",
+  "undefined", // Bet you thought my website was broken ^_^
+  "Use headphones for best experience.",
+  "`base64 /dev/zero` is such a mood.",
+  "My opinions are not my own.",
+  "These fortunes have been cancelled.",
+  "Division is futile. You will be approximated.",
+  ":(){ :|:& };:",
+  "What do you want to debug today?",
+  "I just steal some of these from radare2.",
+  "Causing retinal damage till I figure out dark mode.",
+  "* screams internally *",
   "Let's read Paul Allen's fortunes.",
-  'Free, as in Freedom.',
-  'LinkedIn is great for my mental health!',
-  'Dayman - Fighter of the Nightman',
+  "Free, as in Freedom.",
+  "Just a friendly reminder - Please lint your code.",
+  "Currently solving performance issues that do not exist",
+  "Of course I love Leetcode!",
 ];
 
 // We want to show the same fortune per session, or else the user will be
 // too distracted. I guess. I don't know if anyone will even notice it XD.
 
-const getNewFortune = (ts: number): string => (ts.toString().includes('444')
-  ? 'It is Pooja time.' : fortunes[ts % fortunes.length]);
+const getNewFortune = (ts: number): string =>
+  ts.toString().includes("444")
+    ? "It is Pooja time."
+    : fortunes[ts % fortunes.length];
 
 const getFortune = (): string => {
-  const ts = sessionStorage.getItem('fortune_ts');
+  const ts = sessionStorage.getItem("fortune_ts");
   if (!ts) {
     // No fortune_ts exists, so we add create a new fortune
     const now = Date.now();
-    sessionStorage.setItem('fortune_ts', now.toString());
+    sessionStorage.setItem("fortune_ts", now.toString());
     return getNewFortune(now);
   }
-  return ts.toString().includes('444')
-    ? 'It is Pooja time.'
+
+  return ts.toString().includes("444")
+    ? "It is Pooja time."
     : fortunes[parseInt(ts, 10) % fortunes.length];
 };
 
-export {
-  getFortune,
-};
+export { getFortune };
